7e8713e65a6cd86f9edb1aa0d8f9d0d54d674ef9
CarlosDanielGR <carlos.dgr.12@gmail.com>
2023-05-15 14:24:19 -0500

    feat: ngrx installed correctly

--- projects/archetype/src/main.ts
+++ projects/archetype/src/main.ts
@@ -1,10 +1,20 @@
 /// <reference types="@angular/localize" />
 
 import { bootstrapApplication } from '@angular/platform-browser';
+import { importProvidersFrom } from '@angular/core';
 import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { APP_ROUTES } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { ROOT_REDUCERS } from './app/state/app.state';
 
-const providers = [provideRouter(APP_ROUTES, withPreloading(PreloadAllModules))];
+const providers = [
+  provideRouter(APP_ROUTES, withPreloading(PreloadAllModules)),
+  importProvidersFrom(
+    StoreModule.forRoot(ROOT_REDUCERS),
+    StoreDevtoolsModule.instrument({ name: 'Testing' }),
+  ),
+];
 
 bootstrapApplication(AppComponent, { providers }).catch((err) => console.error(err));
